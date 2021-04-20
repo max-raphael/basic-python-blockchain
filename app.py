2dca34a208149def3c8fb7ca52b72afa5e1660c2
Max Raphael <mrap96@gmail.com>
2021-04-19 23:34:54 -0400

    create basic flask API

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from Blockchain import Blockchain
+import json
+
+app =  Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+app.run(debug=True, port=5000)
